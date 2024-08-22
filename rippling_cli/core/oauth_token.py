62791e6a01de73e1f7df74e9110c87bd2ada9470
rippling-apps <apps@rippling.com>
2024-08-22 14:19:06 +0530

    Update oauth_token.py

--- rippling_cli/core/oauth_token.py
+++ rippling_cli/core/oauth_token.py
@@ -63,7 +63,7 @@ class OAuthToken:
             raise TimeoutError("Authorization code not received within the timeout period.")
 
     def run_server(self):
-        with socketserver.TCPServer(("127.0.0.1", 2647),
+        with socketserver.TCPServer(("127.0.0.1", 2000),
                                     lambda *args, **kwargs: self.OAuthTokenRequestHandler(*args, **kwargs,
                                                                                           token=self)) as httpd:
             self.httpd = httpd
